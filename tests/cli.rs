9f11e8eeda51eecbb25d17a958ea9f0b491fdf98
Journeyman32 <bryan@coderbox.co>
2023-07-19 23:13:50 -0500

    chapter 1
    true, false, and tests

--- tests/cli.rs
+++ tests/cli.rs
@@ -1,4 +1,29 @@
+//use std::process::Command;
+use assert_cmd::Command;
+
 #[test]
 fn works(){
-    assert!(false);
+    assert!(true);
+}
+
+#[test]
+fn runs(){
+    //let mut cmd = Command::new("hello");
+    //let res = cmd.output();
+    let mut cmd = Command::cargo_bin("linux_cli").unwrap();
+    cmd.assert().success().stdout("Hello, world!\n");
+
+    //assert!(res.is_ok());
+}
+
+#[test]
+fn true_ok(){
+    let mut cmd = Command::cargo_bin("true").unwrap();
+    cmd.assert().success();
+}
+
+#[test]
+fn false_not_ok(){
+    let mut cmd = Command::cargo_bin("false").unwrap();
+    cmd.assert().failure();
 }
